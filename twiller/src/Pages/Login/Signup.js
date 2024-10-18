091359735923b55ea3efa813f3a255d649942062
Ani-5604 <151756510+Ani-5604@users.noreply.github.com>
2024-10-18 23:30:39 +0530

    Update Signup.js

--- twiller/src/Pages/Login/Signup.js
+++ twiller/src/Pages/Login/Signup.js
@@ -26,7 +26,7 @@ const Signup = () => {
         name: name,
         email: email,
       };
-      fetch("http://localhost:5000/register", {
+      fetch("https://twitter-twilller-1.onrender.com/register", {
         method: "POST",
         headers: {
           "content-type": "application/json",
