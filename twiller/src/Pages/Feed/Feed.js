024d174cbadc858b70c7074ad829f840f0fa6a95
Ani-5604 <151756510+Ani-5604@users.noreply.github.com>
2024-10-18 23:26:50 +0530

    Update Feed.js

--- twiller/src/Pages/Feed/Feed.js
+++ twiller/src/Pages/Feed/Feed.js
@@ -6,7 +6,7 @@ const Feed = () => {
   const [post, setpost] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/post")
+    fetch("https://twitter-twilller-1.onrender.com/post")
       .then((res) => res.json())
       .then((data) => {
         setpost(data);
